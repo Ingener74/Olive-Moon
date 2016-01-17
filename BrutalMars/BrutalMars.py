a22e8adf2aa7db5452043b41bf48857f20792121
ShnaiderPavel <shnaiderpasha@gmail.com>
2016-01-17 17:16:52 +0300

    Брутальный марс шаблонизирует простейшие состояния по шаблонам

--- BrutalMars/BrutalMars.py
+++ BrutalMars/BrutalMars.py
@@ -1,7 +1,33 @@
 # encoding: utf8
 
 import sys
+from jinja2 import Template
 
 if __name__ == '__main__':
     print u'Brutal Mars - генератор кода машины состояния'
-    print len(sys.argv)
+
+    data = {
+        'events':
+            [
+                {
+                    'name': 'Keyboard',
+                    'argument': {
+                        'type': 'const KeyboardEvent&',
+                        'name': 'keyboardEvent'
+                    }
+                },
+                {
+                    'name': 'Mouse',
+                    'argument': {
+                        'type': 'const MouseEvent&',
+                        'name': 'mouseEvent'
+                    }
+                }
+            ]
+    }
+
+    with open('State.h', 'w') as h:
+        h.write(Template(open('../OliveMoon/templates/State.h.template').read()).render(data))
+
+    with open('State.cpp', 'w') as cpp:
+        cpp.write(Template(open('../OliveMoon/templates/State.cpp.template').read()).render(data))
