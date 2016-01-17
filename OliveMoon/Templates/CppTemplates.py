2e99317f47a454c7cd3f3d7e66badd042a9dfb08
ShnaiderPavel <shnaiderpasha@gmail.com>
2016-01-17 17:48:41 +0300

    решил перенести шаблоны в код на питоне а не в отдельных файлах

---
+++ OliveMoon/Templates/CppTemplates.py
@@ -0,0 +1,123 @@
+# encoding: utf8
+
+STATE_CPP = """
+/*
+Generated file.
+Do not edit.
+*/
+#include "State.h"
+
+State::State(){
+    _state = new State;
+}
+
+State::~State(){
+    delete _state;
+}
+
+void State::onEnter(){
+}
+
+void State::onExit(){
+}
+{% for event in events %}
+void State::handle{{ event.name }} ( {{event.argument.type}} {{event.argument.name}} ){
+    if (_state)
+        _state->handle{{event.name}}({{event.argument.name}});
+}
+{% endfor %}
+void State::switchState(State* state){
+    if(_state)
+        _state->onExit();
+    _state = state;
+    _state->onEnter();
+}
+"""
+
+STATE_H = """
+/*
+Generated file.
+Do not edit.
+*/
+
+#pragma once
+
+class State{
+public:
+    State();
+    virtual ~State();
+
+    virtual void onEnter();
+    virtual void onExit();
+
+    // events
+    {% for event in events %}
+    virtual void handle{{ event.name }} ( {{ event.argument.type }} );
+    {% endfor %}
+    // end events
+
+private:
+    void switchState(State*);
+    State* _state;
+};
+"""
+
+DERIVATIVE_STATE_CPP = """
+/*
+Generated file.
+Do not edit.
+*/
+
+#include "{{state.name}}State.h"
+
+{{state.name}}State::{{state.name}}State(){
+}
+
+{{state.name}}State::~{{state.name}}State(){
+}
+
+{% if %}
+void {{state.name}}State::onEnter(){
+}
+{% endif %}
+{% if %}
+void {{state.name}}State::onExit(){
+}
+{% endif %}
+
+{% for event in events %}
+void {{state.name}}State::handle{{ event.name }} ( {{event.argument.type}} {{event.argument.name}} ){
+    if (_state)
+        _state->handle{{event.name}}({{event.argument.name}});
+}
+{% endfor %}
+
+"""
+
+DERIVATIVE_STATE_H = """
+/*
+Generated file.
+Do not edit.
+*/
+
+#pragma once
+
+#include "State.h"
+
+class {{state.name}}State: public State {
+public:
+    {{state.name}}State();
+    virtual ~{{state.name}}State();
+
+    {% if %}
+    virtual void onEnter();
+    {% endif %}
+    {% if %}
+    virtual void onExit();
+    {% endif %}
+
+    {% for event in events %}
+    virtual void handle{{ event.name }} ( {{ event.argument.type }} );
+    {% endfor %}
+};
+"""
---
+++ OliveMoon/Templates/__init__.py
@@ -0,0 +1,3 @@
+# encoding: utf8
+
+from CppTemplates import (STATE_CPP, STATE_H, DERIVATIVE_STATE_CPP, DERIVATIVE_STATE_H)
